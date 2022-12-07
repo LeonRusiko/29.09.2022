45ed6f83ece90ffb2271fc163fb889a4cd045b11
Aituar Semeibek <113921403+LeonRusiko@users.noreply.github.com>
2022-12-07 22:11:00 +0600

    Update and rename README.md to task 1 .js

---
+++ task 1 .js
@@ -0,0 +1,18 @@
+let car = {
+    manufacturer: "Toyota",
+    model: "Athlete",
+    issueAge: 2018,
+    averageSpeed: 143,
+}
+function showInformation(car) {
+  alert(
+    "Manufacturer: " +
+      manufacturer +
+      "\nModel: " +
+      model +
+      "\nAge of issue: " +
+      issueAge +
+      "\nAverage Speed: " +
+      averageSpeed +
+      "km/h")
+}
